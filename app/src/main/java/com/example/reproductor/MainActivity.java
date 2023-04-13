8163f441660eb0a13b49d5720134e73eb442fd5b
egakipedia <juancarlos.roldan@egaclm.com>
2023-04-13 20:14:52 +0200

    Método Play Pausa

--- app/src/main/java/com/example/reproductor/MainActivity.java
+++ app/src/main/java/com/example/reproductor/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.reproductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button play_pause, btn_repetir;
+    MediaPlayer mp;
+    ImageView iv;
+    int repetir = 2, posicion = 0;
+
+    MediaPlayer vectormp [] = new MediaPlayer[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play_pause = (Button) findViewById(R.id.btn_play);
+        btn_repetir = (Button) findViewById(R.id.btn_repetir);
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        vectormp[0] = MediaPlayer.create(this, R.raw.race);
+        vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+        vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+
+    }
+
+    public void PlayPause (View view){
+
+        if(vectormp[posicion].isPlaying()){
+            vectormp[posicion].pause();
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        }else{
+            vectormp[posicion].start();
+            play_pause.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
     }
 }
